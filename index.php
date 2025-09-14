2a44f18a3940279a41692bf314c5e370f7a369e5
Salama-Shali <roselyne.mwalagho@strathmore.edu>
2025-09-14 23:35:22 +0300

    Implemented bootstrap(css+ js)

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ require 'ClassAutoload.php';
 $ObjLayouts->header($conf);
 $ObjLayouts->navbar($conf);
 $ObjLayouts->banner($conf);
-$ObjLayouts->form_content($conf, $ObjForms);
+$ObjLayouts->content($conf);
 $ObjLayouts->footer($conf);
